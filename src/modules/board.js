2c91f1c3c6ca84271dde9738a81244aa7b21bac5
Esmatullah <esmatqadeemi12@gmail.com>
2023-05-19 12:44:49 +0100

    Use API and also give functionality to the leaderboard project

---
+++ src/modules/board.js
@@ -0,0 +1,30 @@
+import { postData, getData } from "./appi";
+
+function Score(playerScore, PlayerName){
+    this.score = playerScore;
+    this.name = PlayerName;
+}
+
+export default class Board{
+    constructor(){
+        this.scores = []
+        
+    }
+    addPlayerScore = (score, name) =>{
+        const newScore = new Score(score, name)
+        postData(newScore)
+    }
+    getScores = async ()  =>{
+        const scores = await getData();
+        this.scores = scores;
+    }
+    displayScore = () =>{
+        const scoreBoard = document.querySelector(".scores")
+        scoreBoard.innerHTML = '';
+        this.scores.forEach((score) =>{
+            const scoreElement = document.createElement("li");
+            scoreElement.textContent = `${score.user}:${score.score}`;
+            scoreBoard.appendChild(scoreElement)
+        })
+    }
+}
